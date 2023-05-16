14379717d84d3f749fccac9a76aa24cd434e7382
obrMaria <112749225+obrMaria@users.noreply.github.com>
2023-05-16 23:20:35 +0300

    Delete PY directory

--- PY/primer/test_runner.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import calc_tests
-
-calcTestSuite = unittest.TestSuite()
-calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcBasicTests))
-calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcExTests))
-runner = unittest.TextTestRunner(verbosity=2)
-runner.run(calcTestSuite)
--- PY/test_runner.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import ind_test
-
-
-studTestSuite = unittest.TestSuite()
-studTestSuite.addTest(unittest.makeSuite(ind_test.StudTest))
-runner = unittest.TextTestRunner(verbosity=2)
-runner.run(studTestSuite)
